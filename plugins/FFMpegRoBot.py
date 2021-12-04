acdb041d60fd0868afc1970afe903b68730443dd
knbro <81792272+knbro@users.noreply.github.com>
2021-12-04 07:02:03 +0530

    Update FFMpegRoBot.py

--- plugins/FFMpegRoBot.py
+++ plugins/FFMpegRoBot.py
@@ -29,7 +29,7 @@ from hachoir.metadata import extractMetadata
 from hachoir.parser import createParser
 
 
-@Client.on_message(pyrogram.Filters.command(["ffmpegrobot"]))
+@Client.on_message(pyrogram.command(["ffmpegrobot"]))
 async def ffmpegrobot_ad(bot, update):
     TRChatBase(update.from_user.id, update.text, "ffmpegrobot")
     await bot.send_message(
